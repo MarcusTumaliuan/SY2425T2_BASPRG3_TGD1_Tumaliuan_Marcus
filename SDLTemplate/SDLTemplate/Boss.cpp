516f3e23a6ffe2081d004fff31bf40e813c6df62
MarcusTumaliuan <marcusjansel.tumaliuan@benilde.edu.ph>
2025-03-05 14:54:50 +0800

    Milestone 6

---
+++ SDLTemplate/SDLTemplate/Boss.cpp
@@ -0,0 +1,45 @@
+#include "Boss.h"
+
+Boss::Boss()
+{
+}
+
+Boss::~Boss()
+{
+}
+
+void Boss::start()
+{
+}
+
+void Boss::update()
+{
+}
+
+void Boss::draw()
+{
+}
+
+void Boss::SetPlayerTarget(Player* player)
+{
+}
+
+int Boss::GetPositionX()
+{
+	return x;
+}
+
+int Boss::GetPositionY()
+{
+	return y;
+}
+
+int Boss::GetWidth()
+{
+	return width;
+}
+
+int Boss::GetHeight()
+{
+	return height;
+}
